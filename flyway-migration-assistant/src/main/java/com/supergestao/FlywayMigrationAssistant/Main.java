0a47ee569dfa3c159e94c29b2b0883706325fe0f
Neto <neto.ludwig@hotmail.com>
2025-12-27 15:12:00 -0300

    Criado inicio do projeto

---
+++ flyway-migration-assistant/src/main/java/com/supergestao/FlywayMigrationAssistant/Main.java
@@ -0,0 +1,22 @@
+package com.supergestao.FlywayMigrationAssistant;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /*try {
+            // Tenta usar o Look and Feel do sistema operacional
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Se falhar, usa o padrão do Java (Metal)
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
+        });*/
+
+        System.out.println("Olá mundo!");
+    }
+}
